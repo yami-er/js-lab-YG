d688cb60172eee35cea613ad898ead2736e1044a
yamerot gorfu <queenyani@gmail.com>
2020-04-01 15:49:56 -0400

    second commit

--- day3lab.js
+++ day3lab.js
@@ -8,7 +8,8 @@ nextPhoto:function (){
         console.log(this.photoList[this.currentPhotoIndex])
     }
     else{
-        console.log("End of slideshow")
+        console.log("End of slideshow");
+        this.pause();
         }
     }  , 
 prevPhoto:function(){
@@ -17,15 +18,41 @@ prevPhoto:function(){
         console.log(this.photoList[this.currentPhotoIndex])
         }
     else{
-        console.log("End of slideshow")
+        console.log("End of slideshow");
+          this.pause();
         }
      },
 getCurrentPhoto:function(){
     console.log(this.photoList[this.currentPhotoIndex])
-   } 
-  
+   },
+
+playInterval:null,
+
+play:function(){
+    var self = this;
+    this.playInterval = setInterval(function(){self.nextPhoto()}, 2000)
+},
+pause: function() {
+    clearInterval(this.playInterval);
 }
+
+}
+//slideshow.play()
+/* slideshow.play(); */
 /* slideshow.getCurrentPhoto();
 slideshow.nextPhoto();
 //slideshow.nextPhoto();
 slideshow.prevPhoto(); */
+
+/* play: function(){ 
+    //  	clearInterval(); 
+            setInterval(play, 2000);
+            if (this.currentPhotoIndex < this.photoList.length) {
+                 return this.currentPhotoIndex[this.currentPhotoIndex]
+                 this.currentPhotoIndex++;
+             } else {				//after last index, paused slideshow
+                 clearInterval(currentPhotoIndex);
+                 console.log('paused');
+             }
+   }
+   } */
